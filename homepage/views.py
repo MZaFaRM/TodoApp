f74293aba34622b6734f045672e5ced6cfee573a
Muhammed Zafar <98420006+MZaFaRM@users.noreply.github.com>
2023-01-19 04:06:55 +0530

    Update views.py

--- homepage/views.py
+++ homepage/views.py
@@ -3,38 +3,46 @@ from django.contrib.auth.decorators import login_required
 from .models import Task_data
 from datetime import datetime
 import pytz
-from datetime import timezone
 
 
 # Create your views here.
 @login_required(login_url="login")
 def homepage(request):
-    
+
     user_id = request.user.id
+    tasks = Task_data.objects.filter(user_id=user_id).order_by("datetime")
 
     if "task_exist" in request.POST:
         # helps to determine what to do, switch task status or delete task
         quest = request.POST.get("assists")
         element_id = request.POST.get("id")
-        
+
         if quest == "delete_task":
             Task_data.objects.filter(id=element_id).delete()
-            
+
         elif quest == "switch_state":
             task = Task_data.objects.get(id=element_id)
-            Task_data.objects.filter(id=element_id).update(status=(task.status+1) % 2)
+            Task_data.objects.filter(id=element_id).update(status=(task.status + 1) % 2)
 
+    # converting both (now, db saved time) time into same format for comparison
+    now = str(datetime.now())
+    now = now[:19]
+    datetime_object = datetime.strptime(now, "%Y-%m-%d %H:%M:%S")
+    tz = pytz.timezone("Asia/Kolkata")
+    now = tz.localize(datetime_object)
+    
     if "new_task_name" in request.POST:
         # adding new task
+        
         task_name = request.POST.get("new_task_name")
         due_date = request.POST.get("date")
-
+        
         if task_name:
+            
             # form validation
             datetime_object = datetime.strptime(due_date, "%Y-%m-%dT%H:%M")
             tz = pytz.timezone("Asia/Kolkata")
             datetime_object = tz.localize(datetime_object)
-
             task_db = Task_data(
                 user_id=user_id,
                 name=task_name,
@@ -42,15 +50,15 @@ def homepage(request):
                 datetime=datetime_object,
                 status=0,
             )
-            task_db.save()
             
-    tasks = Task_data.objects.filter(user_id=user_id).order_by("datetime")
-    
-    # converting both (now, db saved time) time into same format for comparison
-    now = str(datetime.now())
-    now = now[:19]
-    datetime_object = datetime.strptime(now, "%Y-%m-%d %H:%M:%S")
-    tz = pytz.timezone("Asia/Kolkata")
-    now = tz.localize(datetime_object)
-
+            task_db.save()
+            tasks = Task_data.objects.filter(user_id=user_id).order_by("datetime")
+            
+        else:
+            return render(
+                request,
+                "homepage.html",
+                {"tasks": tasks, "error": "Please provide a name for the task", "now": now}
+            )
+            
     return render(request, "homepage.html", {"tasks": tasks, "now": now})
