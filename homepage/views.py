c6b9e1382835b655517f7549cd950e28ebda3fd4
Muhammed Zafar <98420006+MZaFaRM@users.noreply.github.com>
2023-01-19 03:49:37 +0530

    Update views.py

--- homepage/views.py
+++ homepage/views.py
@@ -9,6 +9,8 @@ from datetime import timezone
 # Create your views here.
 @login_required(login_url="login")
 def homepage(request):
+    
+    user_id = request.user.id
 
     if "task_exist" in request.POST:
         # helps to determine what to do, switch task status or delete task
@@ -23,16 +25,18 @@ def homepage(request):
             Task_data.objects.filter(id=element_id).update(status=(task.status+1) % 2)
 
     if "new_task_name" in request.POST:
+        # adding new task
         task_name = request.POST.get("new_task_name")
         due_date = request.POST.get("date")
 
         if task_name:
-
+            # form validation
             datetime_object = datetime.strptime(due_date, "%Y-%m-%dT%H:%M")
             tz = pytz.timezone("Asia/Kolkata")
             datetime_object = tz.localize(datetime_object)
 
             task_db = Task_data(
+                user_id=user_id,
                 name=task_name,
                 datetime_iso=due_date,
                 datetime=datetime_object,
@@ -40,9 +44,9 @@ def homepage(request):
             )
             task_db.save()
             
-    user_id = request.user.id
     tasks = Task_data.objects.filter(user_id=user_id).order_by("datetime")
     
+    # converting both (now, db saved time) time into same format for comparison
     now = str(datetime.now())
     now = now[:19]
     datetime_object = datetime.strptime(now, "%Y-%m-%d %H:%M:%S")
